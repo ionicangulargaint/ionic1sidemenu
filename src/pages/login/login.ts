9c2676c99c6b0f8319a645b4c7968eaca1d69175
ionicangulargaint <bittusinghbca@gmail.com>
2018-09-17 00:46:13 +0530

    Added sign in page with tab view design

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -11,12 +11,13 @@ import { MainPage } from '../';
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  loginModel: any = 'email';
   // The account fields for the login form.
   // If you're using the username field with or without email, make
   // sure to add it to the type
   account: { email: string, password: string } = {
-    email: 'test@example.com',
-    password: 'test'
+    email: '',
+    password: ''
   };
 
   // Our translated text strings
