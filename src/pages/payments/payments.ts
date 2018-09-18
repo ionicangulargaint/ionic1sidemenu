d7a0c239a6e3b5c8633212adcdebb9aae2c76918
ankitmalik37 <ankitmalik37@gmail.com>
2018-09-19 00:42:09 +0530

    Done payments page UI

---
+++ src/pages/payments/payments.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicPage, MenuController, NavController, Platform } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-payments',
+  templateUrl: 'payments.html'
+})
+export class PaymentsPage {
+  
+  constructor() {}
+
+}
