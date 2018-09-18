5e2a866ffb7e607391764de605e30ce16d79f100
ankitmalik37 <ankitmalik37@gmail.com>
2018-09-19 01:32:01 +0530

    cancel booking done

---
+++ src/pages/cancel-booking/cancel-booking.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicPage, MenuController, NavController, Platform } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-cancel-booking',
+  templateUrl: 'cancel-booking.html'
+})
+export class CancelBookingPage {
+  
+  constructor() {}
+
+}
